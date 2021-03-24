b1333bfb6f19f5397ff218c7e7e49f9ac778911f
daan-bit <dvdrbb33@gmail.com>
2021-03-24 16:14:09 +0100

    update en mainwerken nu

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import serial
 import os
+import mysql.connector
+import time
 
-mydb = mysql.connector.connect{
-    host="web009.zxcs.nl",
+mydb = mysql.connector.connect(
+    host="185.104.29.34",
     user="schoolproject_laravel",
     passwd="kvLXsjsPS",
-    databse="schoolproject_laravel"
-}
+    database="schoolproject_laravel"
+)
 
 port = serial.Serial("/dev/ttyUSB0", 9600, timeout=3.0)
 
 mycursor = mydb.cursor()
 
 while True:
-    rcv = port.readLine().strip()
+    rcv = port.readline().strip()
     if(rcv == 'A'):
         os.system("python update.py")
 
     time.sleep(1)
-    mdb.commit()
+    mydb.commit()
 
 mydb.close()
--- update.py
+++ update.py
@@ -1,12 +1,13 @@
 import mysql.connector
 
-mydb = mysql.connector.connect{
-    host="web009.zxcs.nl",
+mydb = mysql.connector.connect(
+    host="185.104.29.34",
     user="schoolproject_laravel",
     passwd="kvLXsjsPS",
-    databse="schoolproject_laravel"
-}
+    database="schoolproject_laravel"
+)
 
 mycursor = mydb.cursor()
 
 mycursor.execute("UPDATE check_aanwezig SET aanwezig = 'wel';")
+mydb.commit()
