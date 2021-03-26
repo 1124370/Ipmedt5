1915cce681d466ac10d011b458395ff3f172194a
Daniekdegroot <43374493+Daniekdegroot@users.noreply.github.com>
2021-03-26 20:01:07 +0100

    Daniek
    Tabellen aangemaakt

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/', function () {
+Route::get('/timer', function () {
     return view('welcome');
 });
