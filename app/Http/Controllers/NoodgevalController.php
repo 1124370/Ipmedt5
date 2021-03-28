5ddabb78ff710065ec055d2034689b1025d09e3f
daan-bit <dvdrbb33@gmail.com>
2021-03-28 19:39:06 +0200

    bug fix verbding front-end naar arduino

--- app/Http/Controllers/NoodgevalController.php
+++ app/Http/Controllers/NoodgevalController.php
@@ -10,10 +10,10 @@ class NoodgevalController extends Controller
         $noodgeval = \App\Models\Noodgeval::first();
 
         if($noodgeval->noodgeval_on == 'uit'){
-            $noodgeval->noodgeval_on == 'aan';
+            $noodgeval->noodgeval_on = 'aan';
         }
         else{
-            $noodgeval->noodgeval_on == 'uit';
+            $noodgeval->noodgeval_on = 'uit';
         }
         $noodgeval->save();
 
